4c411b67de46d0bae5d8bea0779174ec5d4d2911
Kanishka Gupta <162425328+Kanishka032@users.noreply.github.com>
2026-08-30 12:48:52 +0530

    Time: 2 ms (80.14%) | Memory: 133.8 MB (15.68%) - LeetSync

--- 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.java
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.java
@@ -1,27 +1,15 @@
 class Solution {
     public int longestSubsequence(int[] nums) {
-        int totalXor = 0;
+        int xor = 0;
         boolean hasNonZero = false;
-        
-        for (int num : nums) {
-            totalXor ^= num;
-            if (num != 0) {
-                hasNonZero = true;
-            }
+
+        for (int x : nums) {
+            xor ^= x;
+            if (x != 0) hasNonZero = true;
         }
-        
-        // Case 1: Total XOR is already non-zero. Take the entire array.
-        if (totalXor != 0) {
-            return nums.length;
-        }
-        
-        // Case 2: Total XOR is zero, but we have non-zero elements. 
-        // Drop one non-zero element to make the remaining XOR non-zero.
-        if (hasNonZero) {
-            return nums.length - 1;
-        }
-        
-        // Case 3: The array contains only zeros. No valid subsequence exists.
+
+        if (xor != 0) return nums.length;
+        if (hasNonZero) return nums.length - 1;
         return 0;
     }
 }
